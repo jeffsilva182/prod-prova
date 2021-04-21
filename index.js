9a9bee853fdba83b0797ec5a2060117fe7e35223
jeff.silva182 <jeff.silva182@gmail.com>
2021-04-20 21:37:59 -0300

    correção_2

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const http = require('http');
 const express = require('express');
 const status = require ('http-status');
@@ -27,33 +26,3 @@ sequelize.sync({ force: false }).then(() => {
     const server = http.createServer(app);
     server.listen(process.env.PORT || port);
 });
-=======
-const http = require('http');
-const express = require('express');
-const status = require ('http-status');
-const sequelize = require ('./src/database/database');
-const app = express();
-const routes = require ('./src/routes/routes.js');
-const cors = require('cors');
- 
-app.use(express.json());
- 
-app.use(cors());
- 
-app.use(routes);
- 
-app.use((req, res, next) => {
-    res.status.apply(status.NOT_FOUND).send("Page not found");
-});
- 
-app.use((req, res, next) => {
-    res.status.apply(status.INTERNAL_SERVER_ERROR).json({error});
-});
- 
-sequelize.sync({ force: false }).then(() => {
-    const port = 3003;
-    app.set("port", process.env.PORT || port);
-    const server = http.createServer(app);
-    server.listen(process.env.PORT || port);
-});
->>>>>>> 9c583148d1069f3dabacfad52731b41296025944
